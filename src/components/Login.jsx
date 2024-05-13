b2f40c169001f2bd23b2ec68f561b12900dd2440
bymashihab <khaledshihab221@gmail.com>
2024-05-13 19:51:13 +0600

    added validation and firebase setup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,25 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidateData } from "../utils/validateSignUp";
 
 const Login = () => {
   const [validUser, setValidUser] = useState(true);
+  const [errMsg, setErrMsg] = useState("");
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullname = useRef(null);
+
+  const handleSignUpBtnClick = (e) =>{
+    e.preventDefault();
+  
+    const message = checkValidateData(fullname.current.value,email.current.value, password.current.value);
+ 
+    setErrMsg(message)
+
+    // now go for sign in signup
+
+  }
 
   const handleUserAuth = () => {
     console.log("clicked");
@@ -24,22 +41,29 @@ const Login = () => {
       <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 border  text-white  bg-opacity-80 rounded-lg">
         <h1 className="text-3xl py-4">{validUser ? "Sign In" : "Sign Up"}</h1>
        { !validUser && <input
+        ref={fullname}
           type="text"
           placeholder="Full Name"
           className="p-4 bg-gray-700 my-2  w-full rounded"
         />}
         <input
+        ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 bg-gray-700 my-2  w-full rounded"
         />
+      {/* <p className="text-red-500 text-semibold">{errMsg.toString().includes('Email') ?errMsg : " "}</p> */}
+
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-2 bg-gray-700  w-full rounded"
         />
+          {/* <p className="text-red-500 text-semibold"> {errMsg.toString().includes('Password') ? errMsg : " "} </p> */}
+          <p className="text-red-500 text-semibold"> {errMsg} </p>
 
-        <button className="p-4 my-6 bg-red-500  rounded w-full">
+        <button className="p-4 my-6 bg-red-500  rounded w-full" onClick={handleSignUpBtnClick}>
         {validUser ? "Sign In" : "Sign Up"}
         </button>
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRN926xjbzly6Um10mGE1e20NnLGZp6qU",
+  authDomain: "cinestream-86cd7.firebaseapp.com",
+  projectId: "cinestream-86cd7",
+  storageBucket: "cinestream-86cd7.appspot.com",
+  messagingSenderId: "366680617707",
+  appId: "1:366680617707:web:8a51bd9b322d3d9a3e04ae",
+  measurementId: "G-KN8QQ9BEGB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
